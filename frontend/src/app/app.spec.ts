756e15a1026c946969d28f87c416383c68e6850b
jlobgon <jlobgon@iescarrillo.es>
2026-03-09 12:18:35 +0100

    Refactor:Update AppComponent test to check app-shell

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -15,10 +15,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
+  it('should render the app shell', async () => {
     const fixture = TestBed.createComponent(AppComponent);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend');
+    expect(compiled.querySelector('app-shell')).toBeTruthy();
   });
 });
