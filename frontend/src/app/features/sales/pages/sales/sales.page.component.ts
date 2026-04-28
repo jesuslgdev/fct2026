5c751489b3350eb70944454f3cc4fe3216066de6
jlobgon <jlobgon@iescarrillo.es>
2026-04-28 12:20:08 +0200

    Revert "Merge branch 'feature/frontend_sales_detail' of https://github.com/practicasmb/fct2026 into feature/frontend_sales_detail"
    This reverts commit 2b8502fb39262468d895b551d3ce3c71dc721a2b, reversing
    changes made to ce681c73c2ce97e242c018e76261b734ed5b9ddc.

--- frontend/src/app/features/sales/pages/sales/sales.page.component.ts
+++ frontend/src/app/features/sales/pages/sales/sales.page.component.ts
@@ -91,10 +91,6 @@ export class SalesPageComponent implements OnInit {
     void this.router.navigate(['/sales/new']);
   }
 
-  onViewSale(saleId: number): void {
-    void this.router.navigate(['/sales', saleId]);
-  }
-
   onEditSale(saleId: number): void {
     void this.router.navigate(['/sales', saleId, 'edit']);
   }
